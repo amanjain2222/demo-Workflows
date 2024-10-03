2cf3be6c6eb4bc70da8d73dcb0ae71e4dc0c751d
Aman jain <ajai0035@student.monash.edu>
2024-10-03 14:19:12 +1000

    test

--- main.tf
+++ main.tf
@@ -1,18 +1,10 @@
-terraform {
-      backend "remote" {
-        # The name of your Terraform Cloud organization.
-        organization = "aman-test"
+provider "azurerm" {
+  features {}
+}
 
-        # The name of the Terraform Cloud workspace to store Terraform state files in.
-        workspaces {
-          name = "demo-Workflows"
-        }
-      }
-     }
          # An example resource that does nothing.
-resource "null_resource" "example" {
-      triggers = {
-        value = "A example resource that does nothing!"
-      }
-    }
+resource "azurerm_resource_group" "my-resourseGroup" {
+  name     = "my-resourseGroup"
+  location = "Australia East"
+}
 
